9066470477c18d17a8f9bfb3415e84a80db6732d
Victor Latyshev <v.latyshev@cappasity.com>
2026-02-16 14:45:04 +0300

    feat: support koinovo

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,6 +2,7 @@ export enum APIUrls {
     production = 'https://api.cappasity.com/api/',
     staging = 'https://api.cappasity3d.com/api/',
     tdplatforma = 'https://api.3dplatforma.ru/api/',
+    koinovo = 'https://api.koinovo.ru/api/',
 }
 
 export type ApiTypeKeys = keyof typeof APIUrls;
@@ -10,7 +11,9 @@ export const getApiUrl = (apiType: ApiTypeKeys): string => APIUrls[apiType] ?? A
 
 // Player
 export const API_PLAYER_URL = (apiType: ApiTypeKeys) => `${getApiUrl(apiType)}player`;
-const API_PLAYER_SRC_PATH = (apiType: ApiTypeKeys) => (apiType === 'tdplatforma' ? '3dplatforma-ai' : 'cappasity-ai');
+const API_PLAYER_SRC_PATH = (apiType: ApiTypeKeys) => (
+    apiType === 'tdplatforma' || apiType === 'koinovo' ? '3dplatforma-ai' : 'cappasity-ai'
+);
 export const API_PLAYER_AI_URL = (apiType: ApiTypeKeys) => `${API_PLAYER_URL(apiType)}/${API_PLAYER_SRC_PATH(apiType)}`;
 export const API_PLAYER_SRC = (apiType: ApiTypeKeys, modelID: string, options: string) => (
     `${API_PLAYER_URL(apiType)}/${modelID}/embedded?${options}`
